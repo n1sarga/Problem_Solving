c7a3ef0e2ef2286c30486fa131bfdc8ddcb17563
n1sarga <nextnisarga@gmail.com>
2024-12-05 19:10:48 +0600

    update

--- Basics [Variables, Loops, Conditionals, Functions]/two_sum.py
+++ Basics [Variables, Loops, Conditionals, Functions]/two_sum.py
@@ -5,5 +5,3 @@ def two_sum (nums, target):
         for j in range (i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
-
-print(two_sum(nums=[3, 3], target=6))
