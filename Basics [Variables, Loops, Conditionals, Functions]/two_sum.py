9bde04f66c6d6be2f1514184b1bd60e93800e429
n1sarga <nextnisarga@gmail.com>
2024-12-05 19:12:58 +0600

    update

--- Basics [Variables, Loops, Conditionals, Functions]/two_sum.py
+++ Basics [Variables, Loops, Conditionals, Functions]/two_sum.py
@@ -1,7 +1,8 @@
 '''Problem Link: https://leetcode.com/problems/two-sum/description/'''
 
-def two_sum (nums, target):
-    for i in range (len(nums)):
-        for j in range (i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+class Solution:
+    def two_sum (nums, target):
+        for i in range (len(nums)):
+            for j in range (i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
