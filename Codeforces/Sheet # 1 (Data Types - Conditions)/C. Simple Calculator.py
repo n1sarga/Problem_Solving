bfab88f1d2451ef03a4ff64776cd462c4e83d3ec
n1sarga <nextnisarga@gmail.com>
2025-06-04 23:56:22 +0600

    update

---
+++ Codeforces/Sheet # 1 (Data Types - Conditions)/C. Simple Calculator.py
@@ -0,0 +1,9 @@
+# Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+x, y = input().split()
+x = int(x)
+y = int(y)
+
+print(f'{x} + {y} = {x + y}')
+print(f'{x} * {y} = {x * y}')
+print(f'{x} - {y} = {x - y}')
