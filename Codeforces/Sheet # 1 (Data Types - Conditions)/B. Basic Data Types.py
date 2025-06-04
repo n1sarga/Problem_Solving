3c4e05a4bd9f4500688c7466dd6b5537a563c343
n1sarga <nextnisarga@gmail.com>
2025-06-04 23:53:04 +0600

    update

---
+++ Codeforces/Sheet # 1 (Data Types - Conditions)/B. Basic Data Types.py
@@ -0,0 +1,9 @@
+# Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+a, b, c, d, e = input().split()
+
+print(int(a))
+print(int(b))
+print(c)
+print(float(d))
+print(float(e))
